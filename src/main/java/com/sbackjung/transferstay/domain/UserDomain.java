9ecbccf8eecde548143fd8595d43ce3db3c9925a
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-09-03 17:32:55 +0900

    [#3]feat:JpaAuditing config,설정 추가

--- src/main/java/com/sbackjung/transferstay/domain/UserDomain.java
+++ src/main/java/com/sbackjung/transferstay/domain/UserDomain.java
@@ -1,10 +1,20 @@
 package com.sbackjung.transferstay.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.sql.Timestamp;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name="user")
 public class UserDomain {
     @Id
@@ -38,8 +48,9 @@ public class UserDomain {
     @Column(columnDefinition = "TEXT")
     private String refreshToken;
 
+    @CreatedDate
     private Timestamp createdAt;
-
+    @LastModifiedDate
     private Timestamp updatedAt;
 
     private Timestamp deletedAt;
