6f0ccfef43c337ca8097b39798d53c1e427e09b0
LeeGeonu <rjsdndl909@gmail.com>
2024-09-04 21:06:23 +0900

    Feat : Entity 생성 및 - Assignment_post - Accommodation

---
+++ src/main/java/com/sbackjung/transferstay/domain/AssignmentPost.java
@@ -0,0 +1,73 @@
+package com.sbackjung.transferstay.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "assignment_post")
+public class AssignmentPost {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;  // post_id
+
+
+  @Column(name = "user_id", nullable = false)
+  // 임시고정 userId
+  private Long userId;
+
+  /**
+  * 추후 user 테이블 생성하면 변경하기
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  **/
+  // 숙소 정보
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "accommodation_id", nullable = false)
+  private Accommodation accommodation;
+
+  @Column(nullable = false)
+  private String description;
+
+  @Column(nullable = false)
+  private BigDecimal price;
+
+  @Column(name = "is_auction", nullable = false)
+  private boolean isAuction;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+}
