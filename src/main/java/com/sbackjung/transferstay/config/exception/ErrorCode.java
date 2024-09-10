c22b1ed22c1d8df63b0c5ee7055c392634013f7f
muunioi <muunii06@gmail.com>
2024-09-10 16:58:22 +0900

    [#14] Feat: create CustomException

--- src/main/java/com/sbackjung/transferstay/config/exception/ErrorCode.java
+++ src/main/java/com/sbackjung/transferstay/config/exception/ErrorCode.java
@@ -3,13 +3,12 @@ package com.sbackjung.transferstay.config.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor
 public enum ErrorCode {
 
     // 400 Bad Request
     BAD_REQUEST(400, "잘못된 요청 정보입니다.");
-
 
     private final int code;
     private final String message;
