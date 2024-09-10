0daa721a05e2497768e66f588d47ef25194d73a8
muunioi <muunii06@gmail.com>
2024-09-10 16:58:22 +0900

    [#14] Feat: create CustomException

--- src/main/java/com/sbackjung/transferstay/config/exception/ExceptionAdvice.java
+++ src/main/java/com/sbackjung/transferstay/config/exception/ExceptionAdvice.java
@@ -12,10 +12,27 @@ public class ExceptionAdvice {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<Object> BadRequestException(BadRequestException exception){
+
+        //ErrorCode 에서 미리 Enum 값으로 지정한 코드, 이름 출력
         int code = ErrorCode.BAD_REQUEST.getCode();
         String message = ErrorCode.BAD_REQUEST.getMessage();
+
+        //Exception 을 발생시킨 곳에서 보내는 구체적인 오류 명 (ex. 이메일 정보가 정확하지 않습니다)
         String detail = exception.getMessage();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(code, message, detail));
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Object> CustomException(CustomException exception){
+
+        //ErrorCode 에서 미리 Enum 값으로 지정한 코드, 이름 출력
+        int code = exception.getErrorCode().getCode();
+        String message = exception.getErrorCode().getMessage();
+
+        //Exception 을 발생시킨 곳에서 보내는 구체적인 오류 명 (ex. 이메일 정보가 정확하지 않습니다)
+        String detail = exception.getDetail();
+
+        return ResponseEntity.status(code).body(new ErrorResponse(code, message, detail));
     }
 }
