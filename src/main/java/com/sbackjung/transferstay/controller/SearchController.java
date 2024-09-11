c2fee67a15eaafa0698ff95230a07881af051973
muunioi <muunii06@gmail.com>
2024-09-11 11:55:07 +0900

    [#1] Feat: 검색 기능 구현

--- src/main/java/com/sbackjung/transferstay/controller/SearchController.java
+++ src/main/java/com/sbackjung/transferstay/controller/SearchController.java
@@ -1,16 +1,17 @@
 package com.sbackjung.transferstay.controller;
 
 import com.sbackjung.transferstay.config.exception.BadRequestException;
-import com.sbackjung.transferstay.dto.SearchRequest;
+import com.sbackjung.transferstay.dto.JsonResponse;
+import com.sbackjung.transferstay.dto.SearchResponse;
 import com.sbackjung.transferstay.service.SearchService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.util.StringUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,42 +20,24 @@ public class SearchController {
 
     private final SearchService searchService;
 
-//    @PostMapping("/search")
-//    public String search(@RequestPart SearchRequest.createReq req){
-//
-//        if(req.getFreeField() == null
-//                && req.getCheckIn() == null
-//                && req.getCheckOut() == null
-//                && req.getPerson() == 0
-//                && req.getLocation_depth1() == null
-//                && req.getLocation_depth2()== null) {
-//            return "null";
-//        }
-//
-//        return req.toString();
-//    }
-
     @GetMapping("/search")
-    public String search(){
-        return "hi";
-    }
-
-    @PostMapping("/search")
-    public String searchRequest(@RequestParam(value = "freeField", required = false) String freeField,
-                                @RequestParam(value = "checkIn", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate checkIn,
-                                @RequestParam(value = "checkIn", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate checkOut,
-                                @RequestParam(value = "person", required = false, defaultValue = "0") int person,
-                                @RequestParam(value = "locationDepth1", required = false) String locationDepth1,
-                                @RequestParam(value = "locationDepth2", required = false) String locationDepth2
-                                ){
+    public ResponseEntity<JsonResponse> searchRequest(@RequestParam(value = "freeField", required = false) String freeField,
+                                                      @RequestParam(value = "checkInDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate checkInDate,
+                                                      @RequestParam(value = "checkOutDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate checkOutDate,
+                                                      @RequestParam(value = "personnel", required = false, defaultValue = "0") int personnel,
+                                                      @RequestParam(value = "locationDepth1", required = false) String locationDepth1,
+                                                      @RequestParam(value = "locationDepth2", required = false) String locationDepth2
+    ){
 
         // 모든 파라미터가 null 또는 기본값(예: person의 경우 0)인지 확인
-        if (freeField == null && checkIn == null && checkOut == null && person == 0
+        if (freeField == null && checkInDate == null && checkOutDate == null && personnel == 0
                 && locationDepth1 == null && locationDepth2 == null) {
             throw new BadRequestException("적어도 1개 이상의 필드를 선택해주세요.");
         }
 
-        return freeField+checkIn+checkOut+person+locationDepth1+locationDepth2;
+        List<SearchResponse.getSearchResponse> results = searchService.getSearchResult(freeField, checkInDate, checkOutDate, personnel, locationDepth1, locationDepth2);
+
+        return ResponseEntity.ok(new JsonResponse(200, "검색 완료", results));
 
     }
 }
