2a3a2101e9ed22648c46beaa447c9e51e4022594
muunioi <muunii06@gmail.com>
2024-09-11 12:13:33 +0900

    [#1] Feat: 검색 기능 예외 처리 추가

--- src/main/java/com/sbackjung/transferstay/controller/SearchController.java
+++ src/main/java/com/sbackjung/transferstay/controller/SearchController.java
@@ -1,6 +1,7 @@
 package com.sbackjung.transferstay.controller;
 
-import com.sbackjung.transferstay.config.exception.BadRequestException;
+import com.sbackjung.transferstay.config.exception.CustomException;
+import com.sbackjung.transferstay.config.exception.ErrorCode;
 import com.sbackjung.transferstay.dto.JsonResponse;
 import com.sbackjung.transferstay.dto.SearchResponse;
 import com.sbackjung.transferstay.service.SearchService;
@@ -29,13 +30,35 @@ public class SearchController {
                                                       @RequestParam(value = "locationDepth2", required = false) String locationDepth2
     ){
 
-        // 모든 파라미터가 null 또는 기본값(예: person의 경우 0)인지 확인
+        // Controller 로 넘기기 전 처리해야 할 예외 사항들
+
+        // 1. 모든 파라미터가 null 또는 기본값(예: person의 경우 0)인지 확인
         if (freeField == null && checkInDate == null && checkOutDate == null && personnel == 0
                 && locationDepth1 == null && locationDepth2 == null) {
-            throw new BadRequestException("적어도 1개 이상의 필드를 선택해주세요.");
+
+            throw new CustomException(ErrorCode.BAD_REQUEST, "적어도 1개 이상의 필드를 선택해주세요.");
+        }
+
+        // 2. checkInDate >= checkOutDate 인지 확인
+        if(checkInDate != null && checkOutDate != null){ // 비교를 위해선 두 값이 모두 not null 이여야 함.
+            if(checkInDate.isAfter(checkOutDate)){
+                throw new CustomException(ErrorCode.BAD_REQUEST, "체크인 / 체크아웃 날짜 범위가 잘못 설정되었습니다.");
+            }
+            if(checkInDate.isEqual(checkOutDate)){
+                throw new CustomException(ErrorCode.BAD_REQUEST, "체크인 날짜와 체크아웃 날짜가 동일합니다.");
+            }
+        }
+
+        // 3. locationDepth1 설정 없이 locationDepth2 값만 들어온 경우 -> locationDepth1 > locationDepth2 이기 때문에 선행 조건 필요
+        if(locationDepth1 == null && locationDepth2 != null){
+            throw new CustomException(ErrorCode.BAD_REQUEST, "지역 범위 설정이 잘못되었습니다.");
         }
 
         List<SearchResponse.getSearchResponse> results = searchService.getSearchResult(freeField, checkInDate, checkOutDate, personnel, locationDepth1, locationDepth2);
+
+        if(results.isEmpty()){
+            return ResponseEntity.ok(new JsonResponse(200, "검색 완료", "검색 결과가 존재하지 않습니다."));
+        }
 
         return ResponseEntity.ok(new JsonResponse(200, "검색 완료", results));
 
