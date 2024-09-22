f302a4539eada0f237acb1d8822aeeac897a8dc8
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-09-22 15:55:31 +0900

    feat:Auction 도에민 및 Status 추가

---
+++ src/main/java/com/sbackjung/transferstay/Enum/AuctionStatus.java
@@ -0,0 +1,8 @@
+package com.sbackjung.transferstay.Enum;
+
+public enum AuctionStatus {
+    WAiTING,
+    START,
+    BID_SUCCESS,
+    BID_FAIL
+}
---
+++ src/main/java/com/sbackjung/transferstay/domain/Auction.java
@@ -0,0 +1,45 @@
+package com.sbackjung.transferstay.domain;
+
+import com.sbackjung.transferstay.Enum.AuctionStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicUpdate
+@EntityListeners(EntityListeners.class)
+public class Auction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long actionId;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(nullable = false)
+    private LocalDateTime deadline;
+
+    @Column(nullable = false)
+    private Long startPrice;
+
+    private Long winningBidderId;
+
+    private Long winningPrice;
+
+    @Enumerated(EnumType.STRING)
+    private AuctionStatus status;
+
+}
