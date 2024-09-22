f2b783e8423f6045df48aea150452bc571eb5494
lee <leunwoo000@gmail.com>
2024-09-22 22:24:07 +0900

    feat: swagger 기본설정

--- src/main/java/com/sbackjung/transferstay/config/SecurityConfig.java
+++ src/main/java/com/sbackjung/transferstay/config/SecurityConfig.java
@@ -27,60 +27,61 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtUtils jwtUtils;
-    private final SocialLoginService socialLoginService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler;
-    private final EmailLoginService emailLoginService;
-    private final PasswordEncoder passwordEncoder;
+  private final JwtUtils jwtUtils;
+  private final SocialLoginService socialLoginService;
+  private final OAuth2SuccessHandler oAuth2SuccessHandler;
+  private final EmailLoginService emailLoginService;
+  private final PasswordEncoder passwordEncoder;
 
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http, AuthenticationManager authManager) throws Exception {
-        LoginFilter loginFilter = new LoginFilter(authManager, jwtUtils);
-        loginFilter.setFilterProcessesUrl("/email-login");
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http, AuthenticationManager authManager) throws Exception {
+    LoginFilter loginFilter = new LoginFilter(authManager, jwtUtils);
+    loginFilter.setFilterProcessesUrl("/email-login");
 
-        http
-                .csrf(csrf -> csrf.disable())
-                .formLogin(auth -> auth.disable())
-                .httpBasic(auth -> auth.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers("/", "/login/oauth2/**", "/h2" +
-                                "-console/**", "/auth/**").permitAll()
-                        .requestMatchers("/email-login").permitAll()
-                        .requestMatchers("/api/v1/user/join").permitAll()
-                        .requestMatchers("/api/v1/email/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .redirectionEndpoint(endpoint -> endpoint.baseUri("/login/oauth2/code/{registrationId}"))
-                        .userInfoEndpoint(endpoint -> endpoint.userService(socialLoginService))
-                        .successHandler(oAuth2SuccessHandler)
-                )
-                .exceptionHandling(exception -> exception
-                        .authenticationEntryPoint(authenticationEntryPoint()) // 인증 실패 시 JSON 응답
-                );
+    http
+        .csrf(csrf -> csrf.disable())
+        .formLogin(auth -> auth.disable())
+        .httpBasic(auth -> auth.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
+        .authorizeHttpRequests(request -> request
+            .requestMatchers("/", "/login/oauth2/**", "/h2" +
+                "-console/**", "/auth/**").permitAll()
+            .requestMatchers("/email-login").permitAll()
+            .requestMatchers("/api/v1/user/join").permitAll()
+            .requestMatchers("/api/v1/email/**").permitAll()
+            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .oauth2Login(oauth2 -> oauth2
+            .redirectionEndpoint(endpoint -> endpoint.baseUri("/login/oauth2/code/{registrationId}"))
+            .userInfoEndpoint(endpoint -> endpoint.userService(socialLoginService))
+            .successHandler(oAuth2SuccessHandler)
+        )
+        .exceptionHandling(exception -> exception
+            .authenticationEntryPoint(authenticationEntryPoint()) // 인증 실패 시 JSON 응답
+        );
 
-        http
-                .addFilterBefore(new JwtFilter(jwtUtils), UsernamePasswordAuthenticationFilter.class)
-                .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
+    http
+        .addFilterBefore(new JwtFilter(jwtUtils), UsernamePasswordAuthenticationFilter.class)
+        .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
-    }
+    return http.build();
+  }
 
-    @Bean
-    public AuthenticationEntryPoint authenticationEntryPoint() {
-        return (HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) -> {
-            response.setContentType("application/json;charset=UTF-8");
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write("{\"error\": \"Unauthorized\", " +
-                    "\"message\": \"" + "잘못된 토큰입니다. 혹은 예외처리를 추가해주세요." + "\"}");
-        };
-    }
+  @Bean
+  public AuthenticationEntryPoint authenticationEntryPoint() {
+    return (HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) -> {
+      response.setContentType("application/json;charset=UTF-8");
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      response.getWriter().write("{\"error\": \"Unauthorized\", " +
+          "\"message\": \"" + "잘못된 토큰입니다. 혹은 예외처리를 추가해주세요." + "\"}");
+    };
+  }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 
 }
--- src/main/java/com/sbackjung/transferstay/controller/DbCheckController.java
+++ src/main/java/com/sbackjung/transferstay/controller/DbCheckController.java
@@ -1,20 +1,29 @@
 package com.sbackjung.transferstay.controller;
 
+import com.sbackjung.transferstay.config.exception.CustomException;
+import com.sbackjung.transferstay.config.exception.ErrorCode;
+import com.sbackjung.transferstay.domain.UserDomain;
+import com.sbackjung.transferstay.repository.UserRepository;
+import com.sbackjung.transferstay.utils.UserIdHolder;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class DbCheckController {
     private final DataSource dataSource;
+    private final UserRepository userRepository;
 
     @PostConstruct
     public void checkDbConnectionOnStartup() {
@@ -35,5 +44,4 @@ public class DbCheckController {
             log.error("Database connection failed: {}",e.getMessage());
         }
     }
-
 }
