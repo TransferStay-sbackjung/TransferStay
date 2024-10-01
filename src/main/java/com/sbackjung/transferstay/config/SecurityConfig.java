ee694cae9fd53c0349509088141466c117259206
LeeGeonu <rjsdndl909@gmail.com>
2024-10-01 18:12:51 +0900

    Refactor: API_BASE_URL 수정

--- src/main/java/com/sbackjung/transferstay/config/SecurityConfig.java
+++ src/main/java/com/sbackjung/transferstay/config/SecurityConfig.java
@@ -96,6 +96,7 @@ public class SecurityConfig {
 
     // 허용할 도메인 추가
     configuration.addAllowedOrigin("http://localhost:3000");  // 개발 환경에서 허용
+    configuration.addAllowedOrigin("http://transferstay.p-e.kr:");  // 배포 환경에서 허용
     configuration.addAllowedOrigin("http://transferstay.p-e.kr:3000");  // 배포 환경에서 허용
     configuration.addAllowedMethod("");  // 모든 HTTP 메소드 허용 (GET, POST, PUT, DELETE 등)
     configuration.addAllowedHeader("");  // 모든 헤더 허용
